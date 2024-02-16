179270c15b56a3cf59d83e692607725c74f4751e
Muhammed Ahmed <muhammedahmed167@gmail.com>
2024-02-17 00:18:13 +0500

    Updated main.tf and variables for VPC - Added default values for public_subnet_cidr_blocks - Added default values for private_subnet_cidr_block

--- main.tf
+++ main.tf
@@ -29,29 +29,29 @@ resource "aws_subnet" "public_subnet" {
 }
 
 # Create route tables for public subnets
-resource "aws_route_table" "public" {
-  count  = length(aws_subnet.public)
-  vpc_id = aws_vpc.main.id
-  tags   = merge(var.tags, { Name = "public-${count.index + 1}" })
+resource "aws_route_table" "public_route_table" {
+  count  = length(aws_subnet.public_subnet)
+  vpc_id = aws_vpc.my_vpc.id
+  tags   = merge(var.tags, { Name = "public-route-table-${count.index + 1}" })
 }
 
 # Create a default route in the public route tables through the Internet Gateway
-resource "aws_route" "public" {
-  count               = length(aws_subnet.public)
-  route_table_id      = aws_route_table.public[count.index].id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id          = aws_internet_gateway.main.id
+resource "aws_route" "public_default_route" {
+  count               = length(aws_subnet.public_subnet)
+  route_table_id      = aws_route_table.public_route_table[count.index].id
+  destination_cidr_block = var.public_route_destination
+  gateway_id          = aws_internet_gateway.my_igw.id
 }
 
 # Associate public subnets with the route table
-resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public[count.index].id
+resource "aws_route_table_association" "public_route_table_association" {
+  count          = length(aws_subnet.public_subnet)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table[count.index].id
 }
 
 # Create private subnets
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private_subnet" {
   count             = length(var.private_subnet_cidr_blocks)
   cidr_block        = var.private_subnet_cidr_blocks[count.index]
   vpc_id            = aws_vpc.main.id
--- variables.tf
+++ variables.tf
@@ -28,8 +28,9 @@ variable "create_internet_gateway" {
   default     = true
 }
 variable "public_subnet_cidr_blocks" {
-  description = "List of CIDR blocks for public subnets"
+  description = "(Optional) List of CIDR blocks for public subnets"
   type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "availability_zones" {
@@ -43,11 +44,16 @@ variable "map_public_ip_on_launch" {
   default     = true
 }
 
-
+variable "public_route_destination" {
+  description = "(Optional) Can be provided in case a specific route is needed for internet-bound traffic"
+  type        = string
+  default     = "0.0.0.0/0"
+}
 
 variable "private_subnet_cidr_blocks" {
-  description = "List of CIDR blocks for private subnets"
+  description = "(Optional) List of CIDR blocks for private subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "region" {
